d6bce640656f062b8fb9764eecfcd4a8d3d31421
Tomislav Romic <tomislav.romic@outlook.de>
2024-07-23 23:29:50 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
+    } else if encoded_value.chars().next().unwrap() == 'i' && encoded_value.chars().last().unwrap() == 'e' {
+        let number = &encoded_value[1..encoded_value.len() - 1];
+        return (number.parse::<i64>()).unwrap().into();      
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -33,3 +36,5 @@ fn main() {
         println!("unknown command: {}", args[1])
     }
 }
+
+
