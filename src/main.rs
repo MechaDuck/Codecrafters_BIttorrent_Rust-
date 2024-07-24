8ecd44747ce67cb1587ee8ac84291e6432df2a16
tomislav.romic <tomislav.romic@siemens.com>
2024-07-24 13:11:10 +0200

    removed unused

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ use serde_json;
 use serde_json::Value;
 use std::env;
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
+// Main function to handle command-line arguments and execute commands
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.unwrap().0.to_string());
@@ -18,7 +17,8 @@ fn main() {
     }
 }
 
-fn decode_string(encoded_value: &str) -> Result<(Value, &str), Error>{
+// Function to decode a bencoded string
+fn decode_string(encoded_value: &str) -> Result<(Value, &str), Error> {
     let colon_index = encoded_value.find(':').unwrap();
     let number_string = &encoded_value[..colon_index];
     let number = number_string.parse::<i64>()?;
@@ -26,13 +26,15 @@ fn decode_string(encoded_value: &str) -> Result<(Value, &str), Error>{
     Ok((string.to_string().into(), &encoded_value[colon_index + 1 + number as usize..]))
 }
 
+// Function to decode a bencoded number
 fn decode_number(encoded_value: &str) -> Result<(Value, &str), Error> {
     let end_of_num_index = encoded_value.find('e').unwrap();
     let number = &encoded_value[1..end_of_num_index];
     let parsed_number = number.parse::<i64>()?;
-    Ok((parsed_number.into(), &encoded_value[end_of_num_index + 1..])
-)}
+    Ok((parsed_number.into(), &encoded_value[end_of_num_index + 1..]))
+}
 
+// Function to decode a bencoded dictionary
 fn decode_dictionary(mut encoded_value: &str) -> Result<(Value, &str), Error> {
     encoded_value = &encoded_value[1..];
     let mut serde_json_map = serde_json::Map::new();
@@ -41,47 +43,39 @@ fn decode_dictionary(mut encoded_value: &str) -> Result<(Value, &str), Error> {
             encoded_value = &encoded_value[1..];
             break;
         }
-
+        // decode key
         match decode_bencoded_value(encoded_value) {
             Ok((map_key, remaining)) => {
                 encoded_value = remaining;
                 match decode_bencoded_value(encoded_value) {
                     Ok((value, remaining)) => {
-                        
                         serde_json_map.insert(map_key.as_str().unwrap().to_string(), value);
                         encoded_value = remaining;
-                        if remaining.is_empty(){
+                        if remaining.is_empty() {
                             break;
                         }
-        
                     },
                     Err(e) => {
                         print!("Error decoding dictionary.");
                         return Err(e);
                     }
                 }
-
-                if remaining.is_empty(){
-                    break;
-                }
-
             },
             Err(e) => {
                 print!("Error decoding dictionary.");
                 return Err(e);
             }
         }
-
     }
-    
-    
-    return Ok((serde_json_map.into(), encoded_value));
+    Ok((serde_json_map.into(), encoded_value))
 }
 
+
+
+// Function to decode a bencoded list
 fn decode_list(mut encoded_value: &str) -> Result<(Value, &str), Error> {
     encoded_value = &encoded_value[1..];
     let mut decoded_values = Vec::new();
-
     loop {
         if encoded_value.chars().next().unwrap() == 'e' {
             encoded_value = &encoded_value[1..];
@@ -91,10 +85,6 @@ fn decode_list(mut encoded_value: &str) -> Result<(Value, &str), Error> {
             Ok((value, remaining)) => {
                 decoded_values.push(value);
                 encoded_value = remaining;
-                if remaining.is_empty(){
-                    break;
-                }
-
             },
             Err(e) => {
                 print!("Error decoding list.");
@@ -102,31 +92,27 @@ fn decode_list(mut encoded_value: &str) -> Result<(Value, &str), Error> {
             }
         }
     }
-
-    return Ok((decoded_values.into(), encoded_value));
+    Ok((decoded_values.into(), encoded_value))
 }
 
+// Main decoder function that dispatches to the appropriate decode function
 fn decode_bencoded_value(encoded_value: &str) -> Result<(Value, &str), Error> {
-    // If encoded_value starts with a digit, it's a number
     if encoded_value.chars().next().unwrap().is_digit(10) {
         return decode_string(encoded_value);
-    } 
+    }
     let first_char = encoded_value.chars().next().unwrap();
-    
-    if first_char == 'i'{
+    if first_char == 'i' {
         return decode_number(encoded_value);
-
-    } else if first_char == 'l'{
+    } else if first_char == 'l' {
         return decode_list(encoded_value);
-    } else if first_char == 'd'{
+    } else if first_char == 'd' {
         return decode_dictionary(encoded_value);
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
 
-
-// Tests
+// Tests for the decoding functions
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -135,7 +121,6 @@ mod tests {
     fn test_decode_list_empty() {
         let encoded_value = "le";
         let expected = Value::Array(vec![]);
-
         match decode_bencoded_value(encoded_value) {
             Ok((result, remaining)) => {
                 assert_eq!(result, expected);
@@ -152,7 +137,6 @@ mod tests {
             Value::String("hello".to_string()),
             Value::Number(52.into())
         ]);
-
         match decode_bencoded_value(encoded_value) {
             Ok((result, remaining)) => {
                 assert_eq!(result, expected);
@@ -174,7 +158,6 @@ mod tests {
             ]),
             Value::Number(52.into())
         ]);
-
         match decode_bencoded_value(encoded_value) {
             Ok((result, remaining)) => {
                 assert_eq!(result, expected);
@@ -186,9 +169,9 @@ mod tests {
 
     #[test]
     fn test_decode_dict_nested() {
-        let encoded_value = "d4:testl5:helloi52el5:helloi52eei52eee";
+        let encoded_value = "d4:testd7:in_testl5:helloi52el5:helloi52eei52eee";
         let expected = serde_json::json!({
-            "test": [
+            "test": {"in_test": [
                 "hello",
                 52,
                 [
@@ -196,9 +179,8 @@ mod tests {
                     52
                 ],
                 52
-            ]
+            ]}
         });
-
         match decode_bencoded_value(encoded_value) {
             Ok((result, remaining)) => {
                 assert_eq!(result, expected);
