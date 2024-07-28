6f240f59894cff364a40ed9caafc8996839e8a18
Tomislav Romic <tomislav.romic@outlook.de>
2024-07-28 21:56:19 +0200

    added comments

--- src/torrent_manager/torrent_manager.rs
+++ src/torrent_manager/torrent_manager.rs
@@ -12,19 +12,19 @@ type EncoderFn = dyn Fn(&Value) -> Result<Vec<u8>>;
 // Define a function type for decoding
 type DecoderFn = dyn Fn(&[u8], bool) -> Result<(Value, &[u8])>;
 
+// TorrentManager struct to manage torrent related functionalities
 pub struct TorrentManager<'a> {
-    encoder: &'a EncoderFn,
-    decoder: &'a DecoderFn,
-    metainfo: Option<torrent_spec::meta_info::Metainfo>,
-    tracker_client: Option<clients::tracker_client::TrackerClient>,
-    peers : Option<Vec<torrent_spec::peer_info::Peer>>,
-    peer_client: Option<clients::peer_client::PeerClient>,
+    encoder: &'a EncoderFn,  // Encoder function reference
+    decoder: &'a DecoderFn,  // Decoder function reference
+    metainfo: Option<torrent_spec::meta_info::Metainfo>,  // Optional Metainfo
+    tracker_client: Option<clients::tracker_client::TrackerClient>,  // Optional TrackerClient
+    peers: Option<Vec<torrent_spec::peer_info::Peer>>,  // Optional vector of Peers
+    peer_client: Option<clients::peer_client::PeerClient>,  // Optional PeerClient
 }
 
-
 impl<'a> TorrentManager<'a> {
+    // Constructor for TorrentManager
     pub fn new(encoder: &'a EncoderFn, decoder: &'a DecoderFn) -> Self {
-        
         TorrentManager { 
             encoder, 
             decoder, 
@@ -35,25 +35,31 @@ impl<'a> TorrentManager<'a> {
         }
     }
 
-    pub fn parse_meta_info_file(&mut self, data: Vec<u8>) -> Result<()>{
+    // Parses the meta info file from a byte vector
+    pub fn parse_meta_info_file(&mut self, data: Vec<u8>) -> Result<()> {
+        // Decode the data using the decoder function
         let decoded_value = (self.decoder)(&data, false)?.0;
         let mut metainfo: torrent_spec::meta_info::Metainfo = torrent_spec::meta_info::Metainfo::new();
 
+        // Set various metainfo fields from the decoded data
         metainfo.set_tracker_url(utils::decode_base64_to_utf8_string(decoded_value["announce"].as_str().unwrap()).unwrap());
         metainfo.set_length(decoded_value["info"]["length"].as_i64().unwrap());
         metainfo.set_piece_length(decoded_value["info"]["piece length"].as_i64().unwrap());
-        metainfo.set_piece_hashes( utils::decode_base64_to_hex(decoded_value["info"]["pieces"].as_str().unwrap())?);
+        metainfo.set_piece_hashes(utils::decode_base64_to_hex(decoded_value["info"]["pieces"].as_str().unwrap())?);
         
         let info_data = &decoded_value["info"];
+        // Encode the info data and calculate its SHA1 hash
         let encoded_info = (self.encoder)(info_data)?;
         let hash = utils::calculate_sha1_hash(encoded_info);
         metainfo.set_hash(hash);
 
+        // Set the parsed metainfo to the struct
         self.metainfo = Some(metainfo);
         
         Ok(())
     }
 
+    // Initialize clients such as TrackerClient and PeerClient
     pub fn init_clients(&mut self) -> Result<()> {
         self.is_meta_info_ok()?;
 
@@ -62,10 +68,12 @@ impl<'a> TorrentManager<'a> {
         let length = metainfo.get_length().as_ref().unwrap().clone();
         let info_hash = metainfo.get_hash().as_ref().unwrap().clone();
 
+        // Create a new TrackerClient and request peers
         self.tracker_client = Some(clients::tracker_client::TrackerClient::new(tracker_url));
         let resp = self.tracker_client.as_ref().unwrap().request_peers(length, info_hash).unwrap();
         let decoded_peer_info = (self.decoder)(&resp, false).unwrap().0;
 
+        // Extract peers from the decoded peer info
         let extracted_peers = utils::extract_peers_from_base64_string(decoded_peer_info["peers"].as_str().unwrap().to_string()).unwrap();
         
         let mut peers_vector: Vec<torrent_spec::peer_info::Peer> = vec![];
@@ -77,7 +85,8 @@ impl<'a> TorrentManager<'a> {
         Ok(())
     }
 
-    pub async fn perform_peer_handshake(&self, peer_address: &String)  -> Result<Vec<u8>, Box<dyn Error>>{
+    // Perform handshake with a peer asynchronously
+    pub async fn perform_peer_handshake(&self, peer_address: &String) -> Result<Vec<u8>, Box<dyn Error>> {
         let peer_client = clients::peer_client::PeerClient::new();
         let info_hash = self.metainfo.as_ref().unwrap().get_hash().as_ref().unwrap().clone();
         let info_hash_bytes = utils::hex_to_byte_representation(&info_hash);
@@ -85,33 +94,31 @@ impl<'a> TorrentManager<'a> {
         resp
     }
 
+    // Print the list of peers
     pub fn print_peers(&self) -> Result<()> {
-        if self.peers.is_none(){
+        if self.peers.is_none() {
             return Err(anyhow!("Error: peers were not initialized!"));
         }
 
-        for peer in self.peers.as_ref().unwrap(){
+        for peer in self.peers.as_ref().unwrap() {
             print!("{}\n", peer.get_ip_address());
         }
 
         Ok(())
     }
 
-
-
-    pub fn print_meta_info(&self) -> Result<()>{
+    // Print the meta info
+    pub fn print_meta_info(&self) -> Result<()> {
         self.is_meta_info_ok()?;
         print!("{}", self.metainfo.as_ref().unwrap().get_formatted_info());
         Ok(())
     }
 
+    // Check if the meta info is initialized
     fn is_meta_info_ok(&self) -> Result<()> {
-        if self.metainfo.is_none(){
+        if self.metainfo.is_none() {
             return Err(anyhow!("Error: meta info was not initialized!"));
         }
         Ok(())
     }
-
-
-
 }
